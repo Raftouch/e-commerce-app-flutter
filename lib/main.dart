61fbd4b7129c7eb899485528b3eb03c429c24e1c
Raftouch <rafanelkin@gmail.com>
2024-05-26 18:12:47 +0200

    add intro page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/pages/intro_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: IntroPage(),
     );
   }
 }
