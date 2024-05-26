e3cc9ee3fb9080d671fcd0a3c3c1987f9ff25436
Raftouch <rafanelkin@gmail.com>
2024-05-26 18:48:33 +0200

    add bottom navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/components/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: BottomNavbar(
+        
+      ),
+    );
   }
 }
