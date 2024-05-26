8d645366167528b48bf3f31bbf1cde71d557c54b
Raftouch <rafanelkin@gmail.com>
2024-05-26 18:54:55 +0200

    update home page w/ state

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,12 +9,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+// to control the bottom navbar
+  int _selectedIndex = 0;
+
+// to update the selected index
+// when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       bottomNavigationBar: BottomNavbar(
-        
+        onTabChange: (index) => navigateBottomBar(index),
       ),
     );
   }
