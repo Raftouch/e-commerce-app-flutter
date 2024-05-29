f224b3c955d3b2c553bb5daa9a73f248831dad6f
Raftouch <rafanelkin@gmail.com>
2024-05-29 14:24:44 +0200

    create drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -35,6 +35,54 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: BottomNavbar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Icon(Icons.menu, color: Colors.black));
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // logo
+            Column(
+              children: [
+                DrawerHeader(
+                    child: Image.asset('lib/images/nike_logo.png',
+                        color: Colors.white)),
+                const Padding(
+                  padding: EdgeInsets.only(left: 24.0),
+                  child: ListTile(
+                      leading: Icon(Icons.home, color: Colors.white),
+                      title:
+                          Text('Home', style: TextStyle(color: Colors.white))),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 24.0),
+                  child: ListTile(
+                      leading: Icon(Icons.info, color: Colors.white),
+                      title:
+                          Text('About', style: TextStyle(color: Colors.white))),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 24.0, bottom: 24.0),
+              child: ListTile(
+                  leading: Icon(Icons.logout, color: Colors.white),
+                  title: Text('Logout', style: TextStyle(color: Colors.white))),
+            ),
+            // oter pages
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
