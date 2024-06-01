e1192bfc56d99dd84a556576e02e38a06dc29883
Raftouch <rafanelkin@gmail.com>
2024-06-01 18:24:41 +0200

    add shoe tile component

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/components/shoe_tile.dart';
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -10,8 +11,45 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('S H O P'),
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+              color: Colors.grey[200], borderRadius: BorderRadius.circular(12)),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search', style: TextStyle(color: Colors.grey)),
+              Icon(Icons.search, color: Colors.grey)
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24.0),
+          child: Text('Everyone flies... some fly longer than others',
+              style: TextStyle(color: Colors.grey[600])),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text('Hot picks',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              Text('See all',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.blue))
+            ],
+          ),
+        ),
+        const SizedBox(height: 10),
+        Expanded(child: ListView.builder(itemBuilder: (context, index) {
+          return const ShoeTile();
+        }))
+      ],
     );
   }
 }
