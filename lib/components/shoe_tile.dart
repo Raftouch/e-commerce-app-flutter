8a40a56cc2c356809faa8c72450eba1dea2e52ca
Raftouch <rafanelkin@gmail.com>
2024-06-01 19:18:37 +0200

    style shoe page

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -1,10 +1,68 @@
+import 'package:ecommerce_app/models/shoe.dart';
 import 'package:flutter/material.dart';
 
 class ShoeTile extends StatelessWidget {
-  const ShoeTile({super.key});
+  final Shoe shoe;
+  const ShoeTile({super.key, required this.shoe});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.only(left: 12),
+      width: 200,
+      decoration: BoxDecoration(
+          color: Colors.grey[100], borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // shoe pic
+          ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(shoe.imagePath)),
+          // description
+          Text(
+            shoe.description,
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+          // price + details
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(shoe.name,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '\$' + shoe.price,
+                      style: const TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+                Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(12),
+                            bottomRight: Radius.circular(12))),
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
+          )
+          // button to add to cart
+        ],
+      ),
+    );
   }
 }
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/components/shoe_tile.dart';
+import 'package:ecommerce_app/models/shoe.dart';
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -31,6 +32,7 @@ class _ShopPageState extends State<ShopPage> {
           child: Text('Everyone flies... some fly longer than others',
               style: TextStyle(color: Colors.grey[600])),
         ),
+        const SizedBox(height: 20),
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Row(
@@ -45,10 +47,21 @@ class _ShopPageState extends State<ShopPage> {
             ],
           ),
         ),
-        const SizedBox(height: 10),
-        Expanded(child: ListView.builder(itemBuilder: (context, index) {
-          return const ShoeTile();
-        }))
+        const SizedBox(height: 20),
+        SizedBox(
+            height: 300,
+            child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  // create a shoe
+                  Shoe shoe = Shoe(
+                      name: 'Nike Air',
+                      price: '240',
+                      imagePath: 'lib/images/nike_1.png',
+                      description: 'Cool shoe');
+                  return ShoeTile(shoe: shoe);
+                })),
       ],
     );
   }
